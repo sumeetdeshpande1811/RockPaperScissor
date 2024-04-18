f71aef41d03c4a7044acc2db810ab8990c9a87e7
sumeetdeshpande1811 <deshpande.su@northeastern.edu>
2024-04-18 11:49:57 -0400

    :syringe: adding healthz endpoint

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,12 @@
-const express = require('express');
 
+const dotenv = require("dotenv");
+
+dotenv.config();
+const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
+const healthRoutes=require('./routes/healthRoutes');
 
 
 const app = express();
@@ -10,9 +15,8 @@ const PORT = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 
-
-
 app.use( authRoutes);
+app.use( healthRoutes);
 
 
 app.listen(PORT, () => {
