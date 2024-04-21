655fed09db2f2f264f76b3093eb4ac8dc8376a70
sumeetdeshpande1811 <deshpande.su@northeastern.edu>
2024-04-21 11:33:39 -0400

    :crown: updating model

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  name: { type: String, required: true },
   wins: { type: Number, default: 0 },
   losses: { type: Number, default: 0 },
   draws: { type: Number, default: 0 },
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,11 +7,16 @@ const User = require('../models/User');
 
 router.post('/signup', async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const { username, password,name } = req.body;
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(409).json({ message: 'Username already exists' });
+    }
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ username, password: hashedPassword });
+    const newUser = new User({ username, password: hashedPassword ,name:name});
+    
     await newUser.save();
-    res.status(201).json({ message: 'User created successfully' });
+    return res.status(201).json({ message: 'User created successfully' });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
